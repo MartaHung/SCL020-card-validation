6ae5b55d9623a2d5c5d20df88cd92827f2367f33
katherine <galleguillos.cantuarias@gmail.com>
2022-04-05 22:49:57 -0400

    Enter

--- src/manipulaciondom.js
+++ src/manipulaciondom.js
@@ -2,7 +2,7 @@ let botonvalidar = document.getElementById("botonvalidar");
 
 var tarjeta = document.getElementById("numeroTarjeta")
 
-if (number(tarjeta)=1) {
+if (number(tarjeta)==1) {
     botonvalidar.addEventListener("click", incorrecto);
 } else {
     botonvalidar.addEventListener("click", correcto);
