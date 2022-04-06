9c1afe3df08e081e1ad18f03dbe49546781154b4
katherine <galleguillos.cantuarias@gmail.com>
2022-04-05 22:49:21 -0400

    Enter

--- src/manipulaciondom.js
+++ src/manipulaciondom.js
@@ -1,3 +1,16 @@
-import validator from './validator.js';
+let botonvalidar = document.getElementById("botonvalidar");
 
-console.log(validator);
+var tarjeta = document.getElementById("numeroTarjeta")
+
+if (number(tarjeta)=1) {
+    botonvalidar.addEventListener("click", incorrecto);
+} else {
+    botonvalidar.addEventListener("click", correcto);
+}
+
+
+function incorrecto() {
+    alert("incorrecto")}
+
+function correcto() {
+    alert("correcto")}
