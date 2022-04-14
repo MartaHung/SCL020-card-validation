53a5dc88b72209b615e7905409a990c917548cff
katherine <galleguillos.cantuarias@gmail.com>
2022-04-14 09:55:28 -0400

    Enter

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -11,12 +11,12 @@ describe('validator', () => {
       expect(typeof validator.isValid).toBe('function');
     });
 
-    it('debería retornar true para "4083952015263"', () => {
-      expect(validator.isValid('4083952015263')).toBe(true);
+    it('debería retornar true para "4111111111111111"', () => {
+      expect(validator.isValid('4111111111111111')).toBe(true);
     });
 
-    it('debería retornar true para "79927398713"', () => {
-      expect(validator.isValid('79927398713')).toBe(true);
+    it('debería retornar true para "6011111111111117"', () => {
+      expect(validator.isValid('6011111111111117')).toBe(true);
     });
 
     it('debería retornar false para "1234567890"', () => {
