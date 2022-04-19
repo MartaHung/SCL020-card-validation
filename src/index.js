74ad67dfa1642ab4bf722e2ff314e181619e1595
MartaHung <melhungn@gmail.com>
2022-04-19 10:51:12 -0400

    prueba

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import validator from './validator.js';
+/*console.log(validator); */
 
-console.log(validator);
+let botonvalidar = document.getElementById("botonvalidar");
+
+botonvalidar.addEventListener("click", ()=> {
+
+    let tarjeta = document.getElementById("numero").value;
+    validator(numero);
+}
+
+);
+
+
+
+
+/*let maskify_pant = document.getElementById("botonvalidar");
+
+maskify_pant.addEventListener("click", maskify);
+let numero =  document.getElementById("numero").value;
+maskify(numero); //*/
--- src/validator.js
+++ src/validator.js
@@ -1,23 +1,21 @@
-const validator = {
+/*const validator = {
   // ...
-};
-let botonvalidar = document.getElementById("botonvalidar");
 
-export default validator;
-botonvalidar.addEventListener("click", validar);
 
-function validar() {
-  var tarjeta = document.getElementById("numero").value
 
+/*let botonvalidar = document.getElementById("botonvalidar");
+
+botonvalidar.addEventListener("click", validator);*/
+function validator (cardnumber) {
+ 
+  
   // Aqui escribir funcion de validacion
 
   let suma_tarjeta = 0; // contador de suma elementos tarjeta con condiciones
   let indice = 16; // contador de indice de elementos de tarjeta, reverso
-
+  
   while (tarjeta > 0) { 
-
-    let digito_tarjeta = tarjeta%10;
-
+     let digito_tarjeta = tarjeta%10;
     if (indice%2==0){ //par
       suma_tarjeta = suma_tarjeta + digito_tarjeta;
     } else if (2*digito_tarjeta<10) { //impar de un digito
@@ -26,15 +24,28 @@ function validar() {
       suma_tarjeta = suma_tarjeta + Math.trunc((2*digito_tarjeta)/10) + (2*digito_tarjeta)%10;
     }
 
-    indice = indice - 1;
+    indice = indice - 1;  //resto un numero para poder pasar al siguiente 
     tarjeta = Math.trunc(tarjeta / 10); 
   }
     // condicion final
   if (suma_tarjeta%10==0){
       alert("tarjeta valida");
   } else {
+       document.getElementById('numero').value=""
       alert("tarjeta invalida ¡intenta otra vez!");
   } 
 }
 
-export default validator;
+/*maskify: function (cardnumber)=>{
+  
+  let doceDigitos = numero.slice(0,-4);
+  let ultimosDigitos= numero.slice(-4);
+  let enmascararDigitos = doceDigitos.replace(/./g, "#") + ultimosDigitos;
+  document.getElementById('numero').value=enmascararDigitos;
+  return enmascararDigitos;
+}
+
+};*/
+export default validator; 
+
+
